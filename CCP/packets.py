cdf804e571a151f1470bc27083122cf6d051e644
Alexis Gallèpe <alexis.gallepe@gmail.com>
2016-12-13 11:46:30 +0100

    new pseudo struct for ccp packets

--- CCP/packets.py
+++ CCP/packets.py
@@ -1,8 +1,21 @@
-packet = {
-    "forward":0,
-    "backward":1,
-}
+categories:
+"driving":0
+"alert":1
+
+order:
+init_connection:0
+forward:0
+stop:0
+left:1
+warning:1
+
+args:
+ex: [speed:5, angle:56]
 
 
+call("driving","forward",5)
+
+def get_move_forward(cat="driving", ):
+    return {"categorie":"driving","order":'0',"args":[]}
 
 
--- Car_Controller/controller.py
+++ Car_Controller/controller.py
@@ -7,3 +7,5 @@ def controller(receiving_queue,sending_queue):
     # Treat infos from Images_recognition
     # Treat infos from other cars
     # treat infos from the computer to mosve the car (If leader car only)
+
+
