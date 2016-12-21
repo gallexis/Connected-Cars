86284b336cd91cdaecc2969ca0ce747097ca7377
Alexis Gallèpe <alexis.gallepe@gmail.com>
2016-12-21 21:52:42 +0100

    script connexion.sh to connect to master and create another ad hoc wifi network

--- connexion.sh
+++ connexion.sh
@@ -1,21 +1,57 @@
-#!/bin/env bash
+#!/bin/bash
 
-iw dev wlan0 scan |
-egrep "signal|SSID" |
-sed -e "s/\tsignal: //" -e "s/\tSSID: //" |
-awk '{ORS = (NR % 2 == 0)? "\n" : " "; print}
+#global variables:
+INTERFACE0=wlan0
+INTERFACE1=wlan1
+
+getLastESSID(){
+    local res= iw dev $INTERFACE0 scan | egrep "SSID: wifi_master_" | sort -k 2 -g | tail -1
+    echo res
+}
+
+getNewESSID(){
+    local essid=echo $(getLastESSID)
+    local lastChar=${essid: -1}
+    ((lastChar++))
+    echo wifi_master_${lastChar}
+}
+
+# create adhoc network
+createAdHocNetwork(){
+    local essid=echo $(getNewESSID)
+
+    echo "Creating ad-hoc network..."
+    ifconfig $INTERFACE1 down
+    iwconfig $INTERFACE1 mode ad-hoc
+    iwconfig $INTERFACE1 essid $essid
+    ifconfig $INTERFACE1 192.168.1.1 netmask 255.255.255.0 up
+    echo "Ad-hoc network created"
+}
+
+# connect to wifi
+connect(){
+    local essid=echo $(getLastESSID)
+
+    echo "Trying to connect to Master..."
+    iwconfig $INTERFACE0 essid $essid
+    ifdown $INTERFACE0
+    ifconfig $INTERFACE0 192.168.1.2 netmask 255.255.255.0 up
+    echo "Connected"
+}
+
+echo "================================="
+echo "Master Hotspot creation"
+echo "================================="
+createAdHocNetwork
+
+echo "================================="
+echo "Connection to Master"
+echo "================================="
+connect
 
 
-#create ad hoc network
-iwconfig wlan0 mode Ad-hoc
-iwconfig wlan0 essid MyWifi
 
 
-ifconfig wlan0 192.168.1.1 netmask 255.255.255.0
-ifconfig wlan0 192.168.1.2 netmask 255.255.255.0
-
-
-
-
+exit 0
 
 
