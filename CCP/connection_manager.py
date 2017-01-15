bd84227d62ed4cc398ea719a5fcb4c740917ba87
ignacecool <ignaceagbogba@gmail.com>
2017-01-16 00:15:35 +0100

    convertion

--- CCP/connection_manager.py
+++ CCP/connection_manager.py
@@ -58,10 +58,10 @@ class Master_connection:
         while self.master_alive:
             try:
                 data = self.sock.recv(255)
-                # if sys.getsizeof(data) <= 0:
-                #     print("remote car disconnected")
-                #     self.master_alive = False
-                #     return
+                if sys.getsizeof(data) <= 0:
+                    print("remote car disconnected")
+                    self.master_alive = False
+                    return
                 print("1")
                 msg = CCP.packets.get_message(data)
                 print("2")
