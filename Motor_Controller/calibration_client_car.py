b142eda70fb00426fe5f024a995f17f187345ec2
Alexis Gallèpe <alexis.gallepe@gmail.com>
2017-01-17 12:42:48 +0100

    debug

--- Motor_Controller/calibration_client_car.py
+++ Motor_Controller/calibration_client_car.py
@@ -1,7 +1,7 @@
 import RPi.GPIO as GPIO
-import Motor_Controller.video_dir
-import Motor_Controller.car_dir
-import Motor_Controller.motor
+import video_dir
+import car_dir
+import motor
 import sys
 from socket import *
 from time import ctime
@@ -51,12 +51,12 @@ def setup():
     except:
         print('no config file, set config to original')
 
-    Motor_Controller.video_dir.setup()
-    Motor_Controller.car_dir.setup()
-    Motor_Controller.motor.setup()
+    video_dir.setup()
+    car_dir.setup()
+    motor.setup()
 
-    Motor_Controller.video_dir.calibrate(offset_x, offset_y)
-    Motor_Controller.car_dir.calibrate(offset)
+    video_dir.calibrate(offset_x, offset_y)
+    car_dir.calibrate(offset)
 
 
 def REVERSE(x):
@@ -88,15 +88,15 @@ def loop():
             # --------Motor calibration----------
             if data == 'motor_run':
                 print('motor moving forward')
-                Motor_Controller.motor.setSpeed(50)
-                Motor_Controller.motor.motor0(forward0)
-                Motor_Controller.motor.motor1(forward1)
+                motor.setSpeed(50)
+                motor.motor0(forward0)
+                motor.motor1(forward1)
             elif data[0:9] == 'leftmotor':
                 forward0 = data[9:]
-                Motor_Controller.motor.motor0(forward0)
+                motor.motor0(forward0)
             elif data[0:10] == 'rightmotor':
                 forward1 = data[10:]
-                Motor_Controller.motor.motor1(forward1)
+                motor.motor1(forward1)
 
             # -------------Added--------------
             elif data == 'leftreverse':
@@ -105,64 +105,64 @@ def loop():
                 else:
                     forward0 = "True"
                 print("left motor reversed to", forward0)
-                Motor_Controller.motor.motor0(forward0)
+                motor.motor0(forward0)
             elif data == 'rightreverse':
                 if forward1 == "True":
                     forward1 = "False"
                 else:
                     forward1 = "True"
                 print("right motor reversed to", forward1)
-                Motor_Controller.motor.motor1(forward1)
+                motor.motor1(forward1)
             elif data == 'motor_stop':
                 print('motor stop')
-                Motor_Controller.motor.stop()
+                motor.stop()
             # ---------------------------------
 
             # -------Turing calibration------
             elif data[0:7] == 'offset=':
                 offset = int(data[7:])
-                Motor_Controller.car_dir.calibrate(offset)
+                car_dir.calibrate(offset)
             # --------------------------------
 
             # ----------Mount calibration---------
             elif data[0:8] == 'offsetx=':
                 offset_x = int(data[8:])
                 print('Mount offset x', offset_x)
-                Motor_Controller.video_dir.calibrate(offset_x, offset_y)
+                video_dir.calibrate(offset_x, offset_y)
             elif data[0:8] == 'offsety=':
                 offset_y = int(data[8:])
                 print('Mount offset y', offset_y)
-                Motor_Controller.video_dir.calibrate(offset_x, offset_y)
+                video_dir.calibrate(offset_x, offset_y)
             # ----------------------------------------
 
             # -------Turing calibration 2------
             elif data[0:7] == 'offset+':
                 offset = offset + int(data[7:])
                 print('Turning offset', offset)
-                Motor_Controller.car_dir.calibrate(offset)
+                car_dir.calibrate(offset)
             elif data[0:7] == 'offset-':
                 offset = offset - int(data[7:])
                 print('Turning offset', offset)
-                Motor_Controller.car_dir.calibrate(offset)
+                car_dir.calibrate(offset)
             # --------------------------------
 
             # ----------Mount calibration 2---------
             elif data[0:8] == 'offsetx+':
                 offset_x = offset_x + int(data[8:])
                 print('Mount offset x', offset_x)
-                Motor_Controller.video_dir.calibrate(offset_x, offset_y)
+                video_dir.calibrate(offset_x, offset_y)
             elif data[0:8] == 'offsetx-':
                 offset_x = offset_x - int(data[8:])
                 print('Mount offset x', offset_x)
-                Motor_Controller.video_dir.calibrate(offset_x, offset_y)
+                video_dir.calibrate(offset_x, offset_y)
             elif data[0:8] == 'offsety+':
                 offset_y = offset_y + int(data[8:])
                 print('Mount offset y', offset_y)
-                Motor_Controller.video_dir.calibrate(offset_x, offset_y)
+                video_dir.calibrate(offset_x, offset_y)
             elif data[0:8] == 'offsety-':
                 offset_y = offset_y - int(data[8:])
                 print('Mount offset y', offset_y)
-                Motor_Controller.video_dir.calibrate(offset_x, offset_y)
+                video_dir.calibrate(offset_x, offset_y)
             # ----------------------------------------
 
             # ----------Confirm--------------------
@@ -180,7 +180,7 @@ def loop():
                 fd.write(config)
                 fd.close()
 
-                Motor_Controller.motor.stop()
+                motor.stop()
                 tcpSerSock.close()
                 quit()
             else:
