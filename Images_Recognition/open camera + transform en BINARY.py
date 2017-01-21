fba045b022a4015f2ee6d0a8244d2be407b41327
kmakhlouf <kaced.makhlouf@gmail.com>
2017-01-21 15:11:44 +0100

    Update open camera + transform en BINARY.py

--- Images_Recognition/open camera + transform en BINARY.py
+++ Images_Recognition/open camera + transform en BINARY.py
@@ -15,15 +15,10 @@ print width
 
 while True:
     ret,im = cap.read()
-
-    # Blur image to remove noise
-#    img_filter = cv2.GaussianBlur(im.copy(), (3, 3), 0)
     
     gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-    #retval, threshold = cv2.threshold(im, 12, 255, cv2.THRESH_BINARY)
 
     th = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 115, 1)
-
 
     cv2.imshow('My camera' ,im)
     cv2.imshow('Binary camera', th)
