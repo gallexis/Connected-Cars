03368e4ce2d80f576c9aa939db0d71c7994986c1
Alexis Gallèpe <alexis.gallepe@gmail.com>
2017-02-02 16:57:43 +0100

    added heiritage in the sub controllers

--- Main_Controller/sub_controllers/Master_controller.py
+++ Main_Controller/sub_controllers/Master_controller.py
@@ -2,7 +2,7 @@ import logging
 
 from Main_Controller.global_queues import *
 from Main_Controller.sub_controllers.Generic_sub_controller import *
-
+from CCP.packets import create_message
 
 class Master_controller(Generic_sub_controller):
     def __init__(self):
@@ -13,7 +13,7 @@ class Master_controller(Generic_sub_controller):
 
     def driving_manager(self):
         # send the same driving order to the slave
-        TO_SLAVE_Q.put(self.data)
+        TO_SLAVE_Q.put(create_message(self.type, self.order, self.args))
 
         # send the order "move_forward" to the motors
         TO_MOTORS_Q.put((self.order, self.args))
